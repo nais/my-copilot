4ed92370ca84932dcca1c4508649095837fef2ce
Hans Kristian Flaatten <hans.kristian.flaatten@nav.no>
2025-01-10 18:20:43 +0100

    Debug all headers

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,8 +4,10 @@ import { headers } from "next/headers";
 export default async function Home() {
 
   const headersList = headers();
-  const authorizationHeader = (await headersList).get("Authorization");
-  console.log(authorizationHeader);
+  const allHeaders = await headersList;
+  allHeaders.forEach((value, key) => {
+    console.log(`${key}: ${value}`);
+  });
 
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
