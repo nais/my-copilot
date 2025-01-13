0d060a191430967dd2a8df66ef840651ede2ed6c
Hans Kristian Flaatten <hans.kristian.flaatten@nav.no>
2025-01-13 18:28:51 +0100

    Add better auth debugging

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -7,6 +7,10 @@ type User = {
   email: string;
 };
 
+// aud = AZURE_APP_CLIENT_SECRET
+// iss = AZURE_OPENID_CONFIG_ISSUER
+// oid = AZURE_APP_CLIENT_ID
+
 const loginEndpoint = '/oauth2/login';
 
 const publicKey = process.env.AZURE_APP_JWKS;
@@ -24,24 +28,27 @@ export async function validateJsonWebToken(token: string): Promise<boolean> {
     const decodedHeader = JSON.parse(Buffer.from(header, "base64").toString());
     const decodedPayload = JSON.parse(Buffer.from(payload, "base64").toString());
 
+    console.log("decodedHeader", decodedHeader);
+    console.log("decodedPayload", decodedPayload);
+
     // Verify the algorithm
     if (decodedHeader.alg !== "RS256") {
-      throw new Error("Invalid algorithm");
+      throw new Error(`Invalid algorithm. Expected: RS256, got: ${decodedHeader.alg}`);
     }
 
     // Verify the audience
     if (decodedPayload.aud !== process.env.AZURE_APP_CLIENT_SECRET) {
-      throw new Error("Invalid audience");
+      throw new Error(`Invalid audience. Expected: ${process.env.AZURE_APP_CLIENT_SECRET}, got: ${decodedPayload.aud}`);
     }
 
     // Verify the issuer
     if (decodedPayload.iss !== process.env.AZURE_OPENID_CONFIG_ISSUER) {
-      throw new Error("Invalid issuer");
+      throw new Error(`Invalid issuer. Expected: ${process.env.AZURE_OPENID_CONFIG_ISSUER}, got: ${decodedPayload.iss}`);
     }
 
     // Verify the public key
     if (!publicKey) {
-      throw new Error("Public key is not defined");
+      throw new Error(`Public key is not defined in environment variables`);
     }
 
     // Verify the signature using Web Crypto API
