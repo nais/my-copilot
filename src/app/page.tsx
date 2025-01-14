891d3567f2a603eba385f6c0dbac6743ef859489
Hans Kristian Flaatten <hans.kristian.flaatten@nav.no>
2025-01-14 10:18:02 +0100

    Debug the header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,13 +2,6 @@ import Image from "next/image";
 import { headers } from "next/headers";
 
 export default async function Home() {
-
-  const headersList = headers();
-  const allHeaders = await headersList;
-  allHeaders.forEach((value, key) => {
-    console.log(`${key}: ${value}`);
-  });
-
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -105,6 +105,8 @@ export async function getUser(shouldRedirect: boolean = true): Promise<User | nu
     return null;
   }
 
+  console.log("Auth header:", authHeader);
+
   const token = authHeader.replace("Bearer ", "");
   const isValid = await validateJsonWebToken(token);
   if (!isValid) {
